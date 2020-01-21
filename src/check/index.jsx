384a0291f42103fa1e75b2e3103b1793dd32e84a
吴文怡 <wuwy@yczcjk.com>
2020-01-21 16:53:06 +0800

    -feat:登录页

---
+++ src/check/index.jsx
@@ -0,0 +1,4 @@
+/** check * */
+import React from 'react';
+
+
---
+++ src/login/index.jsx
@@ -0,0 +1,146 @@
+/** Login * */
+import React from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import box from '../assets/img/box.png';
+import logo from '../assets/img/logo.png';
+import miniLogo from '../assets/img/logo_blue.png';
+import {login} from '../server/api';
+import 'antd/dist/antd.css';
+import './style.scss';
+/*import Header from './header';
+import Footer from './footer';
+import Register from './register';
+import VerifyAccount from './forgetPassword/verifyAccount';
+import WriteCode from './forgetPassword/writeCode';
+import ChangePassword from './forgetPassword/changePassword';
+/!*import {
+	bankConf, // 个性配置
+} from '@/utils/api/user';*!/
+// import rsaEncrypt from '@/utils/encryp';
+// import { Button } from '@/components';*/
+
+// ==================
+// 所需的所有组件
+// ==================
+const loginForm = Form.create;
+class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			type: 1,
+			phoneNum: '',
+			btnColor: '#1C80E1',
+			imgUrl: undefined,
+			imgLoading: false,
+		};
+	}
+/*
+	componentDidMount() {
+		bankConf().then((_res) => {
+			if (_res.code === 200) {
+				this.setState({
+					btnColor: _res.data.btnColor,
+					imgUrl: _res.data.url,
+				});
+			} else {
+				this.setState({
+					imgLoading: true,
+				});
+			}
+		});
+	}*/
+
+	handleSubmit = e =>{
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log('Received values of form: ', values);
+			}
+			login(values).then(res => {
+				if(res.data.code === 200){
+					console.log('success')
+				}
+			});
+		});
+	};
+
+	render() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+		<div className="yc-login">
+			<div style={{ opacity: 0, height: 0, display: 'none' }}>
+				<input type="text" />
+				<input type="password" />
+			</div>
+			<div className="yc-login-header">
+				<div className="header-img">
+					<img src={logo} alt="" />
+				</div>
+			</div>
+			<div className="yc-login-container">
+				<div className="yc-container-body">
+					<div className="yc-left">
+						<img src={box} width="650" height="600" alt="" />
+					</div>
+					<div className="yc-right">
+						<p className="yc-form-title">用户登录</p>
+						<Form onSubmit={this.handleSubmit} className="login-form">
+							<Form.Item>
+								{getFieldDecorator('username', {
+									rules: [{ required: true, message: 'Please input your username!' }],
+								})(
+									<Input
+										className="yc-input"
+										prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+										placeholder="请输入账号"
+									/>,
+								)}
+							</Form.Item>
+							<Form.Item>
+								{getFieldDecorator('password', {
+									rules: [{ required: true, message: 'Please input your Password!' }],
+								})(
+									<Input
+										className="yc-input"
+										prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+										type="password"
+										placeholder="请输入密码"
+									/>,
+								)}
+							</Form.Item>
+							<Form.Item style={{marginTop:-20}}>
+								{getFieldDecorator('remember', {
+									valuePropName: 'checked',
+									initialValue: true,
+								})(<Checkbox className="yc-forget" style={{marginLeft:6, fontSize:12}}>下次自动登录</Checkbox>)}
+								<a className="yc-forget" href="" style={{marginLeft:145}}>
+									忘记密码
+								</a>
+								<Button type="primary" htmlType="submit" className="yc-login-button">
+									登录
+								</Button>
+							</Form.Item>
+						</Form>
+					</div>
+				</div>
+				<div className="yc-login-footer">
+					<div className="footer-container">
+						<div className="footer-tech">
+							<img src={miniLogo}  alt="" />
+							<span style={{marginLeft:6}}>杭州源诚科技有限公司 技术支持</span>
+						</div>
+						<div className="footer-copyright">
+							<span>
+								Copyright©2019杭州源诚科技有限公司 备案号：浙ICP备17030D14
+							</span
+							>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		);
+	}
+}
+
+export default loginForm()(Login);
