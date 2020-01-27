f59bb9252ff603a1e91ed79e4475b0fec7cd67e2
wwy <wuwy@yczcjk.com>
2020-01-27 19:36:41 +0800

    -feat:路由跳转,原生 route 数组对象（not yet

--- src/pages/App.js
+++ src/pages/App.js
@@ -23,6 +23,33 @@ class App extends React.Component {
 		)
 	}
 }
+/*替换的配置方式
+因为 route 一般被嵌套使用，
+所以使用 JSX 这种天然具有简洁嵌套型语法的结构来描述它们的关系非常方便。
+然而，如果你不想使用 JSX，也可以直接使用原生 route 数组对象。
+
+const routeConfig = [
+	{ path: '/',
+		component: App,
+		indexRoute: { component: Dashboard },
+		childRoutes: [
+			{ path: 'about', component: About },
+			{ path: 'inbox',
+				component: Inbox,
+				childRoutes: [
+					{ path: '/messages/:id', component: Message },
+					{ path: 'messages/:id',
+						onEnter: function (nextState, replaceState) {
+							replaceState(null, '/messages/' + nextState.params.id)
+						}
+					}
+				]
+			}
+		]
+	}
+]
+
+React.render(<Router routes={routeConfig} />, document.body)*/
 
 export default App;
 
