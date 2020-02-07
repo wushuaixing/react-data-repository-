b5a3436cbc48f1058a53af8b2a9168e430846762
wwy <wuwy@yczcjk.com>
2020-02-07 16:34:39 +0800

    -feat:[结／检／管]-基本信息;

--- src/pages/structure/detail/index.jsx
+++ src/pages/structure/detail/index.jsx
@@ -206,8 +206,38 @@ class  StructureDetail extends React.Component {
 	}
 
 	goBack=()=>{};
-	toSave=()=>{
+	toSave=()=>{};
 
+	// const date_format = date => {/* your code */}
+	//拍卖状态
+	filterAuctionStatus=(value) =>{
+		if (value === 1) {
+			return "即将开始";
+		} else if (value === 3) {
+			return "拍卖中";
+		} else if (value === 5) {
+			return "成功交易";
+		} else if (value === 7) {
+			return "失败";
+		} else if (value === 9) {
+			return "终止";
+		} else if (value === 11) {
+			return "撤回";
+		}
+	};
+
+	//打开标题链接
+	openLink=()=>{
+		let href = window.location.href.split("#")[0];
+		// window.open(href + "#/SourcePage/"+this.$route.params.id);
+	};
+
+	//
+	associated=(id) =>{
+		let href = window.location.href.split("#")[0];
+		window.open(
+			href + "#/check/" + id + "/" + strucData.data[0].auctionStatus
+		);
 	};
 
 
@@ -216,6 +246,7 @@ class  StructureDetail extends React.Component {
     const { }=this.props;
     const { dataMark, dataTotal, buttonText, buttonStyle }=this.state;
     const { errorReason,recordsForCheck }=this.state;
+    const basic=strucData.data[0];
         return(
           <div>
             <div className="yc-detail-title">
@@ -233,6 +264,36 @@ class  StructureDetail extends React.Component {
 								>{buttonText}
 							  </Button>
 	            </div>
+							<div className="yc-wrong-part">
+								<div className="yc-part-title">
+									<p>基本信息</p>
+								</div>
+								<div className="yc-wrong-detail">
+									<div>
+										<p className="yc-sec-title">标题:</p>
+										<p className="yc-link-title" onClick={this.openLink} style={{ marginLeft:5 }} >{ basic.title }</p>
+								  </div>
+									<div>
+										<p className="yc-sec-title">拍卖状态:</p>
+										<p className="yc-sec-title" style={{ marginLeft:5}}>{ this.filterAuctionStatus(basic.auctionStatus) }</p>
+									</div>
+									{/*//什么数据是有撤回原因和关联标注的 条件：!character && status !== 0*/}
+								<div >
+									<p className="yc-sec-title">撤回原因:</p>
+									<p className="yc-sec-title" style={{ marginLeft:5}}>{ basic.reasonForWithdrawal }</p>
+								</div>
+								<div >
+									<p className="yc-sec-title">关联标注:</p>
+									<p
+										className="yc-link-title"
+										style={{ marginLeft:5}}
+									  onClick={this.associated(basic.associatedAnnotationId)}
+								>
+									{basic.associatedAnnotationId }
+								</p>
+							</div>
+						</div>
+					</div>
 							<WrongReason errorList={errorReason} />
 							<StructureRecord records={recordsForCheck} />
 						</div>
