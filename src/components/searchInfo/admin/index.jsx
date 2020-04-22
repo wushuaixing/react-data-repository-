2818b36ce054fc98afa468d898627568a3d7d330
likun <likun@yczcjk.com>
2020-04-22 16:28:04 +0800

    -feat:管理员资产结构化检查表格 筛选栏逻辑全部完成（人员筛选）

--- src/components/searchInfo/admin/index.jsx
+++ src/components/searchInfo/admin/index.jsx
@@ -1,8 +1,6 @@
-/** search **/
-
-//筛选栏 管理员能通过地区(省/市/区)以及抓取时间,结构化人员和检查人员进行表格筛选。
+//筛选栏 管理员能通过地区(省/市/区) 抓取时间 还有人员筛选
 import React from 'react';
-import { Form, Input, Button, DatePicker, Cascader, Select, message } from 'antd';
+import { Form, Input, DatePicker, Cascader, Select, message } from 'antd';
 import { getStructuredPersonnel, getCheckPersonnel } from "../../../server/api";
 import { area } from "../../../assets/area";
 import { dateUtils } from "../../../utils/common";
@@ -11,61 +9,18 @@ import { SearchAndClearButtonGroup } from '../../common/index'
 const { Option, OptGroup } = Select;
 const searchForm = Form.create;
 class Index extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userList: [],
-            checkUserList: [],
-            area,
-        };
-    }
-
+    state = {
+        userList: [],
+        checkUserList: [],
+        area,
+    };
     componentDidMount() {
-        //结构化人员
-        getStructuredPersonnel("").then(res => {
+        //结构化人员列表(管理员获取)
+        getStructuredPersonnel().then(res => {
             if (res.data.code === 200) {
-                let id = res.data.data[0]["firstNameRank"];
-                let list = [];
-                let typeList = [];
-                typeList.push({
-                    id: "",
-                    array: [
-                        {
-                            value: 0,
-                            label: "全部"
-                        },
-                        {
-                            value: 1,
-                            label: "已删除"
-                        },
-                        {
-                            value: 2,
-                            label: "自动标注"
-                        }
-                    ]
-                });
-
-                for (let key = 0; key < res.data.data.length; key++) {
-                    if (res.data.data[key]["firstNameRank"] === id) {
-                        list.push({
-                            value: res.data.data[key]["id"],
-                            label: res.data.data[key]["name"]
-                        });
-                    } else {
-                        typeList.push({
-                            id: id,
-                            array: list
-                        });
-                        id = res.data.data[key]["firstNameRank"];
-                        list = [];
-                        list.push({
-                            value: res.data.data[key]["id"],
-                            label: res.data.data[key]["name"]
-                        });
-                    }
-                }
+                let data = res.data.data
                 this.setState({
-                    userList: typeList,
+                    userList: this.getStructuredPersonnelTypeList(data),
                 });
             } else {
                 message.error(res.data.message);
@@ -74,19 +29,9 @@ class Index extends React.Component {
         //检查人员列表(管理员获取)
         getCheckPersonnel().then(res => {
             if (res.data.code === 200) {
-                let checklist = [];
-                checklist.push({
-                    value: "all",
-                    label: "全部"
-                });
-                for (let key = 0; key < res.data.data.length; key++) {
-                    checklist.push({
-                        value: res.data.data[key]["id"],
-                        label: res.data.data[key]["name"]
-                    });
-                }
+                let data = res.data.data
                 this.setState({
-                    checkUserList: checklist,
+                    checkUserList: this.getCheckPersonnelList(data),
                 })
             } else {
                 message.error(res.data.message);
@@ -103,29 +48,50 @@ class Index extends React.Component {
             page: 1
         }
         Object.keys(formParams).forEach((key) => {
-            //判断各种情况为空及area(特殊处理)  清理空参数
-            if (formParams[key] !== null && formParams[key] !== '' && key !== 'area') {
+            //判断各种情况为空 清理空参数
+            if (formParams[key] !== null && formParams[key] !== '' && formParams[key].length !== 0) {
+                //如果选择了地点级联  对应赋值参数
+                if (key === 'area') {
+                    const location = formParams[key]
+                    for (let i = 0; i < formParams[key].length; i++) {
+                        switch (i) {
+                            case 0:
+                                params.region = location[0]; break;
+                            case 1:
+                                params.city = location[1]; break;
+                            case 2:
+                                params.area = location[2]; break;
+                            default:
+                                break;
+                        }
+                    }
+                }
                 //如果是日期 把Moment处理掉
-                params[key] = (key !== 'endTime' && key !== 'startTime') ? formParams[key] : dateUtils.formatMomentToStandardDate(formParams[key])
-            }
-            //如果选择了地点级联  对应赋值参数
-            if (key === 'area' && formParams[key].length > 0) {
-                const location = formParams[key]
-                for (let i = 0; i < formParams[key].length; i++) {
-                    switch (i) {
-                        case 0:
-                            params.region = location[0]; break;
-                        case 1:
-                            params.city = location[1]; break;
-                        case 2:
-                            params.area = location[2]; break;
+                else if (key === 'endTime' || key === 'startTime') {
+                    params[key] = dateUtils.formatMomentToStandardDate(formParams[key])
+                }
+                //如果是结构化人员ID  选择了三个特殊类型 判断类型值不是数字 则对应赋值userType 
+                //isNaN()判断的缺点就在于 null、空格以及空串会被按照0来处理 但外层已经处理
+                else if (key === 'userId' && isNaN(parseInt[formParams[key]])) {
+                    console.log(formParams[key])
+                    switch (formParams[key]) {
+                        case 'all':
+                            params.userType = 0; break;
+                        case 'deleted':
+                            params.userType = 1; break;
+                        case 'auto':
+                            params.userType = 2; break;
                         default:
                             break;
                     }
                 }
+                //无特殊情况 正常赋值
+                else {
+                    params[key] = formParams[key]
+                }
             }
         })
-        //console.log(params)
+        console.log(params)
         this.props.toSearch(params);
     };
 
@@ -134,7 +100,58 @@ class Index extends React.Component {
         this.props.form.resetFields();
         this.props.toClear();
     };
-    //根据所处tabIndex获取搜索时间栏的文字显示内容 然后传给搜索栏子组件
+
+    getStructuredPersonnelTypeList(data) {
+        let typeMark = data[0]["firstNameRank"]  //类名
+        let typeList = [{
+            id: "用户类型",
+            array: [
+                {
+                    value: 'all',
+                    label: "全部"
+                },
+                {
+                    value: 'deleted',
+                    label: "已删除"
+                },
+                {
+                    value: 'auto',
+                    label: "自动标注"
+                }
+            ]
+        }] //类名数组 
+        let tempList = [] //类名数组的子集  负责暂时存放
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].firstNameRank !== typeMark) {
+                typeList.push({
+                    id: typeMark,
+                    array: tempList
+                })
+                typeMark = data[i].firstNameRank;
+                tempList = [];
+            }
+            tempList.push({
+                value: data[i].id,
+                label: data[i].name
+            })
+        }
+        return typeList;
+    }
+    getCheckPersonnelList(data) {
+        let checklist = [];
+        checklist.push({
+            value: "",
+            label: "全部"
+        });
+        for (let i = 0; i < data.length; i++) {
+            checklist.push({
+                value: data[i].id,
+                label: data[i].name
+            });
+        }
+        return checklist;
+    }
+    //根据所处tabIndex获取搜索时间栏的文字显示内容
     get searchTimeTypeInput() {
         switch (this.props.tabIndex) {
             case 0: case 1:
@@ -187,11 +204,11 @@ class Index extends React.Component {
                     </Form.Item>
                     <Form.Item label="结构化人员">
                         {getFieldDecorator('userId', {
-                            initialValue: ''
+                            initialValue: null
                         })(
                             <Select style={{ width: 198, marginLeft: 4 }} transfer placeholder="请选择">
                                 {
-                                    userList && userList.map((item, index) => {
+                                    userList.map((item, index) => {
                                         return (
                                             <OptGroup label={item.id} key={index}>
                                                 {item.array.map((ele, index) => {
@@ -213,11 +230,11 @@ class Index extends React.Component {
                     </Form.Item>
                     <Form.Item label="检查人员">
                         {getFieldDecorator('checkUserId', {
-                            initialValue: ''
+                            initialValue: null
                         })(
                             <Select style={{ width: 198, marginLeft: 4 }} transfer placeholder="请选择">
                                 {
-                                    checkUserList && checkUserList.map((item) => {
+                                    checkUserList.map((item) => {
                                         return (
                                             <Option
                                                 value={item.value}
