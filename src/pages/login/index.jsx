5ec48448499367b7d3904957e13471581aebad18
async <yuanlb@yczcjk.com>
2020-06-19 17:36:01 +0800

    -fix [login] temp-user

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -136,7 +136,7 @@ class Login extends React.Component {
 		});
 	};
 	render() {
-		const { getFieldDecorator } = this.props.form;
+		const { getFieldDecorator,setFieldsValue:set } = this.props.form;
 		const { codeImgSrc, loading, errorCount, showForm } = this.state;
 		return (
 			<Spin tip="Loading..." spinning={loading}>
@@ -220,8 +220,15 @@ class Login extends React.Component {
 											</Button>
 										</Form.Item>
 									</Form>
+									<div className='temp-module'>
+										<Button onClick={()=>set({username:'18358141463',password:'123456'})}>结构化管理人员</Button>
+										<Button onClick={()=>set({username:'55555555555',password:'555555'})}>检查人员</Button>
+										<Button onClick={()=>set({username:'15386196726',password:'111111'})}>结构化人员</Button>
+									</div>
 								</div>
 							}
+
+
 							{
 								showForm === 'findPassword' &&
 								<ForgetPasswordForm handleSwitchBack={this.switchForm.bind(this)} resetPasswordSuccess={this.handleResetPasswordSuccess.bind(this)}/>
