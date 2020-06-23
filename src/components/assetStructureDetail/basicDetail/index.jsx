fe8aea0cbaaa3ab1754b10d02a991d8209e3cf42
async <yuanlb@yczcjk.com>
2020-06-23 20:38:51 +0800

    -fix [bug] remove console.log

--- src/components/assetStructureDetail/basicDetail/index.jsx
+++ src/components/assetStructureDetail/basicDetail/index.jsx
@@ -6,7 +6,6 @@ import { Link, withRouter } from "react-router-dom";
 //功能还剩链接跳转  在父组件补上Prop和回调逻辑即可  另外要根据传入prop选择需要显示的row
 const StructureBasicDetail = (props) => {
     const { associatedAnnotationId, associatedStatus } = props;
-    console.log(props);
     let associatedAnnotationRoute,autoMarkRoute = '';
     if (associatedAnnotationId) {
         switch (localStorage.getItem('userState')) {
@@ -86,7 +85,6 @@ const BasicDetailRow = (props) => {
 
 function StructureRecord(props) {
     const { index, record } = props;
-    console.log(record);
     let classType = 'structure-record';
     let temp = null;
     let userType = 0;
