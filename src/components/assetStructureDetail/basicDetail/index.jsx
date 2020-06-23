ec93731fd15937c4e3bd2e2a116573cf609e396a
async <yuanlb@yczcjk.com>
2020-06-23 20:38:25 +0800

    -fix [bug] 【ID1007344】【检查账号】既是非初标又被自动标注的已标记数据详情页展示问题。

--- src/components/assetStructureDetail/basicDetail/index.jsx
+++ src/components/assetStructureDetail/basicDetail/index.jsx
@@ -5,49 +5,52 @@ import { Link, withRouter } from "react-router-dom";
 
 //功能还剩链接跳转  在父组件补上Prop和回调逻辑即可  另外要根据传入prop选择需要显示的row
 const StructureBasicDetail = (props) => {
-    const { associatedAnnotationId, associatedStatus } = props
+    const { associatedAnnotationId, associatedStatus } = props;
+    console.log(props);
     let associatedAnnotationRoute,autoMarkRoute = '';
     if (associatedAnnotationId) {
         switch (localStorage.getItem('userState')) {
             case '管理员':
-                associatedAnnotationRoute = `/notFirstMark/admin/${associatedAnnotationId}`; 
-                autoMarkRoute = `/autoMark/admin/${props.associatedAnnotationId}`
+                associatedAnnotationRoute = `/notFirstMark/admin/${associatedAnnotationId}`;
+                autoMarkRoute = `/autoMark/admin/${props.associatedAnnotationId}`;
                 break;
             case '检查人员':
                 associatedAnnotationRoute = `/notFirstMark/check/${associatedAnnotationId}`;
-                autoMarkRoute = `/autoMark/check/${props.associatedAnnotationId}`
+                autoMarkRoute = `/autoMark/check/${props.associatedAnnotationId}`;
                 break;
             case '结构化人员':
-                associatedAnnotationRoute = `/notFirstMark/structure/${associatedAnnotationId}/${associatedStatus}`; 
+                associatedAnnotationRoute = `/notFirstMark/structure/${associatedAnnotationId}/${associatedStatus}`;
                 break;
             default:
                 break;
         }
     }
+    // 是否属于自动标注的记录
+    const hasAuto = (props.records||[]).some(i=>i.desc==='自动标注');
     return (
         <div className="yc-components-assetStructureDetail" id="yc-components-basicDetail">
             <div className="yc-components-assetStructureDetail_header">基本信息</div>
             <div className="yc-components-basicDetail_body">
-                <BasicDetailRow title={'标题'} content={props.title} to={`/auctionDetail/${props.auctionID}`}></BasicDetailRow>
-                <BasicDetailRow title={'拍卖状态'} content={AUCTION_STATUS[props.auctionStatus]}></BasicDetailRow>
+                <BasicDetailRow title={'标题'} content={props.title} to={`/auctionDetail/${props.auctionID}`}/>
+                <BasicDetailRow title={'拍卖状态'} content={AUCTION_STATUS[props.auctionStatus]}/>
                 {
                     (props.auctionStatus === 9 || props.auctionStatus === 11) ?
-                        <BasicDetailRow title={'撤回原因'} content={props.reasonForWithdrawal?props.reasonForWithdrawal:'-'}></BasicDetailRow> :
+                        <BasicDetailRow title={'撤回原因'} content={props.reasonForWithdrawal?props.reasonForWithdrawal:'-'}/> :
                         null
                 }
                 {
-                    props.type === 2 ?
-                        <BasicDetailRow title={'关联标注'} content={'链接'} to={associatedAnnotationRoute}></BasicDetailRow> :
+                    props.type === 2 && (associatedAnnotationRoute && !hasAuto)?
+                        <BasicDetailRow title={'关联标注'} content={'链接'} to={associatedAnnotationRoute}/> :
                         null
                 }
                 {
                     props.records && props.records.length > 0 ?
-                        <BasicDetailRow title={'结构化/检查记录'} content={props.records} autoMarkRoute={autoMarkRoute}></BasicDetailRow> : null
+                        <BasicDetailRow title={'结构化/检查记录'} content={props.records} autoMarkRoute={autoMarkRoute}/> : null
                 }
             </div>
         </div>
     )
-}
+};
 
 const BasicDetailRow = (props) => {
     return (
@@ -65,7 +68,7 @@ const BasicDetailRow = (props) => {
                                     props.content.map((record, index) => {
                                         return (
                                             <span className={'yc-components-assetStructureDetail_body-row_content'} key={index} style={index !== 0 ? { marginLeft: 113.4, display: 'inline-block', marginTop: 5 } : null}>
-                                                <StructureRecord record={record} key={index} index={index} autoMarkRoute={props.autoMarkRoute}></StructureRecord>
+                                                <StructureRecord record={record} key={index} index={index} autoMarkRoute={props.autoMarkRoute}/>
                                             </span>
                                         )
 
@@ -79,16 +82,17 @@ const BasicDetailRow = (props) => {
             }
         </div>
     )
-}
+};
 
 function StructureRecord(props) {
-    const { index, record } = props
-    let classType = 'structure-record'
+    const { index, record } = props;
+    console.log(record);
+    let classType = 'structure-record';
     let temp = null;
     let userType = 0;
     if (record.desc === '结构化') {
         temp = index === 0 ? '初次结构化':'修改';
-    } 
+    }
     else if (record.desc === '自动标注'){
         temp = '自动标注';
     }
