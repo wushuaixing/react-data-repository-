603e5b45ef4446b747d4f53b3cce45ea2243abdb
xiyu <zhangy@yczcjk.com>
2020-09-13 19:01:27 +0800

    -fixbug[ID1009387]角色信息行问题/信息展示错误

--- src/components/assetStructureDetail/roleDetail/autoCompleteInput.jsx
+++ src/components/assetStructureDetail/roleDetail/autoCompleteInput.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {message,AutoComplete} from 'antd';
+import {message,AutoComplete,Input} from 'antd';
 import {getAutoPrompt} from '../../../server/api';
 
 class autoCompleteInput extends Component {
@@ -8,38 +8,27 @@ class autoCompleteInput extends Component {
         this.state={
             prompstList:[],//所有姓名输入框 提示语的集合
             paramsLengthList:[],//所有姓名输入框  输入内容的集合
-            isBlur:''
-        }
-        this.handChange = this.deBounce(this.handChange, 500)
-    }
-    deBounce(fn, ms) {
-        let timeoutId
-        return function () {
-            clearTimeout(timeoutId)
-            timeoutId = setTimeout(() => {
-                fn.apply(this, arguments)
-            }, ms)
+            isBlur:'',
+            isChinese:'' //输入法是否为中文
         }
     }
     handChange(...rest){     
-        console.log(...rest);
         const combine=rest[0]||'';   
         const isBlur=rest[1]
         this.setState({
             isBlur
         })
         const params=rest[2]||''.trim();
-        console.log(combine,params);
         this.props.handleNameChange(combine,params);
         const arr_index=combine.substr(combine.length - 1, 1);//索引
-        this.getAutoPrompt(params,arr_index) //输入框值发生改变时发送请求
+        this.state.isChinese!=="Chinese"&&this.getAutoPrompt(params,arr_index) //输入框值发生改变时发送请求
     }
     getAutoPrompt(params,index){
         const list = ['银行', '信用社', '信用联社', '合作联社', '合作社'];
         const flag = list.some(item => params.includes(item));//名称中不包括“银行、信用社、信用联社、合作联社、合作社
         let prompstList=this.state.prompstList;
         let paramsLengthList=this.state.paramsLengthList;
-        let param=params.replace(/<span style='color:red'>/g,'').replace(/<\/span>/g,'')
+        let param=params.replace(/<span style='color:red'>/g,'').replace(/<\/span>/g,'').trim()
         if (param.length > 3 && !flag) {   //角色名称大于等于四个字
             getAutoPrompt(param).then(res => {
                 if (res.data.code === 200) {
@@ -68,22 +57,37 @@ class autoCompleteInput extends Component {
             })
         }
     }
-    UNSAFE_componentWillReceiveProps(){ 
+    judgeChinese=(e)=>{
+        if(e.type==='compositionstart'){
+            this.setState({
+                isChinese:'Chinese'
+            })
+        }
+        if(e.type==='compositionend'){
+            let val=e.target.value;
+            let index=e.target.name;
+            this.setState({
+                isChinese:'English'
+            },()=>{
+                this.getAutoPrompt(val,index)
+            })
+        }
+    }
+
+    componentDidMount(){ 
         const {obligor,index}=this.props;
         this.getAutoPrompt(obligor.name,index); //props发生改变时拿到index和name值发送请求 
-     }
-     componentWillUnmount(){
-         this.setState=()=>false   //不能在组件销毁后设置state，防止出现内存泄漏  (防抖设置定时器)
      }
 
     render() {
         const {prompstList,paramsLengthList,isBlur}=this.state;
         const {obligor,disabled,index}=this.props;
         const options=(prompstList[index]||[]).map((item)=>{
+            let value=item.replace(/<span style='color:red'>/g,'').replace(/<\/span>/g,'');//onselect的默认参数结果就是Option的key值
             return (
-            <AutoComplete.Option key={item} text={item.replace(/<span style='color:red'>/g,'').replace(/<\/span>/g,'')}>
-               <div dangerouslySetInnerHTML={{ __html:item}}></div>
-            </AutoComplete.Option>)
+            <AutoComplete.Option key={value} text={value}>
+               <div dangerouslySetInnerHTML={{ __html:item}}></div>  
+            </AutoComplete.Option>)   
            
         })
 
@@ -91,15 +95,22 @@ class autoCompleteInput extends Component {
             <div className="auto_complete_content">
                 <AutoComplete
                     dataSource={options} //显示的5条自动提示
-                    defaultValue={obligor.name}//默认值
+                    value={obligor.name}//默认值
                     disabled={disabled}
                     defaultActiveFirstOption={false}//是否默认高亮第一个选项
                     placeholder="请输入名称"
                     onChange={this.handChange.bind(this,`name${index}`,'onChange')}
                     onBlur={this.handChange.bind(this,`name${index}`,'onBlur')}
                     className={paramsLengthList[index]&&isBlur==='onBlur'?'atuo_complete_nodata':'atuo_complete'}// 未匹配到对应的工商信息时边框为黄色
-                    optionLabelProp='text'
-                />
+                    optionLabelProp='text'     //回填到选择框的 Option 的属性值，默认是 Option 的子元素
+                >
+                    <Input 
+                        onCompositionStart={this.judgeChinese} //使用拼音输入法开始输入汉字时，这个事件就会被触发
+                        onCompositionEnd={this.judgeChinese}   //拼音输入法输入汉字 按下空格键时 触发
+                        name={index}
+                        autoComplete='off'
+                    />
+                </AutoComplete>
 
                 {
                     paramsLengthList[index]&&isBlur==='onBlur'?<p className="auto_complete_nodata">未匹配到对应的工商信息</p>:null
