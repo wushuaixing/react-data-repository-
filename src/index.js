7ea24c3e327189c62486502b347ac2df135e1794
async <yuanlb@yczcjk.com>
2020-09-18 17:55:23 +0800

    -fix [version]

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import './index.css';
 moment.locale('zh-cn');
 
 const Version = 'v1.2.1';
-const BetaNumber = '.2';
+const BetaNumber = '.3';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
