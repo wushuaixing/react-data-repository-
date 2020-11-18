b0b28484d381e00593e37c210213a70f119132e8
xiyu <zhangy@yczcjk.com>
2020-11-18 09:57:09 +0800

    -feat 修改版本号

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import './index.css';
 
 moment.locale('zh-cn');
 
-const Version = 'v1.3.0';
-const BetaNumber = '.10';
+const Version = 'v1.4.0';
+const BetaNumber = '.0';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
